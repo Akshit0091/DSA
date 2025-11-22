ab4337ef2fed675548854232567fcf98d9e190b6
Akshit Pathak <pathakakshit17@gmail.com>
2025-11-23 02:35:19 +0530

    Implement diagonal traversal of a matrix

---
+++ Diagonal_Traverse.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        vector<int> result;
+        result.reserve(m * n);
+        
+        int row = 0, col = 0;
+        bool upward = true;
+        
+        while (result.size() < m * n) {
+            result.push_back(mat[row][col]);
+            if (upward) {
+                if (col == n - 1) {
+                    row++;
+                    upward = false;
+                } else if (row == 0) {
+                    col++;
+                    upward = false;
+                } else {
+                    row--;
+                    col++;
+                }
+            } else {
+                if (row == m - 1) {
+                    col++;
+                    upward = true;
+                } else if (col == 0) {
+                    row++;
+                    upward = true;
+                } else {
+                    row++;
+                    col--;
+                }
+            }
+        }
+        
+        return result;
+    }
+};
