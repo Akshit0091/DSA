e6aff42bbe3c43bbe6a2e93aed051ba4baa90e0b
Akshit Pathak <pathakakshit17@gmail.com>
2026-01-30 16:24:58 +0530

    Implement reverse string function with k segments

---
+++ Reverse String II.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    void solve(string& s, int& k,int index){
+        if(index >= s.size()){
+            return;
+        }
+
+        int start = index;
+        int end = min(index+k-1,(int)s.size()-1);
+        while(start < end){
+            swap(s[start++],s[end--]);
+        }
+        solve(s,k,index + (2*k));
+    }
+
+    string reverseStr(string s, int k) {
+        int index = 0;
+        solve(s,k,index);
+        return s;
+    }
+};
