585522c59dde7c62c3e315a9e1e1947fbdf08098
Akshit Pathak <pathakakshit17@gmail.com>
2026-04-24 15:22:50 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (30.54%) - LeetSync

--- 3019-furthest-point-from-origin/furthest-point-from-origin.cpp
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.cpp
@@ -3,20 +3,13 @@ public:
     int furthestDistanceFromOrigin(string moves) {
         int n = moves.length();
         int distL = 0;
-        int distR = 0;
+        int blanks = 0;
         for(int i=0; i<n; i++){
             if(moves[i] == 'L') distL--;
             else if(moves[i] == 'R') distL++;
-            else distL--; 
+            else blanks++; 
         }
 
-        for(int i=0; i<n; i++){
-            if(moves[i] == 'L') distR--;
-            else if(moves[i] == 'R') distR++;
-            else distR++; 
-        }
-        int distanceR = abs(0 - distR);
-        int distanceL = abs(0 - distL);
-        return max(distanceL, distanceR);
+        return abs(distL) + blanks;
     }
 };
